8ed0828fd319c39c5c26c3adf161e0167efcd4ee
CaleeAdmin <admin@calee.com.au>
2026-02-15 20:50:01 +0800

    Exclude NextCloud group from local calendars page

--- lib/feature/local_calendars_page.dart
+++ lib/feature/local_calendars_page.dart
@@ -30,7 +30,9 @@ class LocalCalendarsPage extends StatelessWidget {
           return const Center(child: CircularProgressIndicator());
         }
 
-        final groups = ctrl.calendarGroups;
+        final groups = ctrl.calendarGroups
+            .where((group) => group.accountName != 'NextCloud')
+            .toList();
         if (groups.isEmpty) {
           return const Center(child: Text('No local calendars found'));
         }
