8ed307842196142c052c177f17ba429c741c959a
CaleeAdmin <admin@calee.com.au>
2026-02-17 12:21:16 +0800

    fix(android): correct and align app namespace/applicationId

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.nextcloud.caleesync"
+    namespace = "com.viso.caleesync"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "ccom.nextcloud.caleesync"
+        applicationId = "com.viso.caleesync"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
