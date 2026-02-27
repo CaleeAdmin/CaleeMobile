44af42051db8923ddbdd1a6ef53222afb037ecaa
CaleeAdmin <admin@calee.com.au>
2026-02-27 13:08:23 +0800

    Fix linked-state detection on device calendar page

--- lib/controllers/local_calendar_page_controller.dart
+++ lib/controllers/local_calendar_page_controller.dart
@@ -74,19 +74,17 @@ class LocalCalendarPageController extends GetxController {
       }
 
       final db = await DatabaseHelper.instance.database;
-      final String? loginName = MMKVUtils.instance.getString(AppConstant.loginNameKey);
-
       final List<Map<String, dynamic>> rows = await db.rawQuery('''
         SELECT lb.local_collection_id
         FROM local_bindings lb
-        INNER JOIN remote_collections rc ON rc.id = lb.remote_collection_id
         WHERE lb.local_collection_id IS NOT NULL
           AND lb.local_collection_id != ''
-          AND rc.account_name = ?
-      ''', [loginName ?? '']);
+      ''');
       final Set<String> connectedLocalIds = {
         for (final row in rows) row['local_collection_id'].toString(),
       };
+
+      final String? loginName = MMKVUtils.instance.getString(AppConstant.loginNameKey);
 
       final List<Map<String, dynamic>> remoteProvisionedRows = await db.rawQuery('''
         SELECT lb.local_collection_id
