4744dc11db9c20864787b6f388396aa752f138de
CaleeAdmin <admin@calee.com.au>
2026-03-20 19:50:56 +0800

    Revert "Align iOS marker padding with 6-char length (#385)"
    This reverts commit 2f5d56fdb15cbea87f4b72413e32925fc72546c1.

--- lib/data/sync_repository.dart
+++ lib/data/sync_repository.dart
@@ -108,8 +108,8 @@ class SyncRepository {
         .toUnsigned(32)
         .toRadixString(16)
         .toUpperCase()
-        .padLeft(6, '0')
-        .substring(0, 6);
+        .padLeft(8, '0')
+        .substring(0, 8);
     return marker.replaceAll(RegExp(r'[^A-Z0-9]'), '');
   }
 
