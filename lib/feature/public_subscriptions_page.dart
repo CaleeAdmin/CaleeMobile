57e41b15631a07e412edea825f7f804e194125df
CaleeAdmin <admin@calee.com.au>
2026-03-22 19:12:48 +0800

    Fix calendar navigation build errors (#465)

--- lib/feature/public_subscriptions_page.dart
+++ lib/feature/public_subscriptions_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import '../controllers/CalendarPageController.dart';
 import '../controllers/public_subscriptions_controller.dart';
 import '../entity/public_subscription.dart';
 
